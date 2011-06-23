0216ab0a48736b0a9e2307fc3ac3c5e0e9109130
Franco Catena <francocatena@gmail.com>
2011-06-23 00:32:27 -0300

    Agregado de nodos en la edición de diapositivas

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,11 +13,63 @@ module ApplicationHelper
     javascript_include_tag *libs
   end
   
+  # Returns a string with an object identifier
+  # 
+  # * _prefix_:: The prefix to append in the ID
+  # * _form_builder_:: The form builder object
+  def dynamic_object_id(prefix, form_builder)
+    "#{prefix}_#{form_builder.object_name.to_s.gsub(/[_\]\[]+/, '_')}"
+  end
+  
   # Return the error messages from a model in HTML format
+  # 
+  # * _model_:: The model to inspect for errors
   def show_error_messages(model)
     unless model.errors.empty?
       render :partial => 'shared/error_messages', :locals => { :model => model }
     end
+  end
+  
+  def generate_html(form_builder, method, user_options = {})
+    options = {
+      :object => form_builder.object.class.reflect_on_association(method).klass.new,
+      :partial => method.to_s.singularize,
+      :form_builder_local => :f,
+      :locals => {},
+      :child_index => 'NEW_RECORD',
+      :is_dynamic => true
+    }.merge(user_options)
+    form_options = { :child_index => options[:child_index] }
+
+    form_builder.fields_for(method, options[:object], form_options) do |f|
+      render(
+        :partial => options[:partial],
+        :locals => {
+          options[:form_builder_local] => f,
+          :is_dynamic => options[:is_dynamic]
+        }.merge(options[:locals])
+      )
+    end
+  end
+
+  def generate_template(form_builder, method, options = {})
+    escape_javascript generate_html(form_builder, method, options)
+  end
+  
+  # Returns a link to remove a nested item
+  #
+  # * _fields_:: The fields_for builder
+  # * _class_for_remove_:: Optional HTML class name to use for remove
+  def link_to_remove_nested_item(fields = nil, class_for_remove = nil)
+    new_record = fields.nil? || fields.object.new_record?
+    out = String.new
+    out << fields.hidden_field(:_destroy, :class => :destroy,
+      :value => fields.object.marked_for_destruction? ? 1 : 0) unless new_record
+    out << link_to('X', '#', :title => t(:'label.delete'),
+      :'data-target' => ".#{class_for_remove || fields.object.class.name.underscore}",
+      :'data-event' => (new_record ? 'removeItem' : 'hideItem'))
+
+    raw out
   end
   
   # Returns the pagination links for the objects collection
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,85 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+var State = {
+  newIdCounter: 0
+}
+
+var EventHandler = {
+  addNestedItem: function(e) {
+    var template = eval(e.data('template'));
+
+    $(e.data('container')).append(Util.replaceIds(template, /NEW_RECORD/g));
+
+    e.trigger('item:added', e);
+  },
+  
+  hideItem: function(e) {
+    Helper.hide($(e).parents($(e).data('target')));
+
+    $(e).prev('input[type=hidden].destroy').val('1');
+
+    $(e).trigger('item:hidden', $(e));
+  },
+
+  removeItem: function(e) {
+    var target = e.parents(e.data('target'));
+
+    Helper.remove(target);
+
+    target.trigger('item:removed', target);
+  }
+}
+
+var Helper = {
+  hide: function(element, callback) {
+    $(element).stop().slideUp(500, callback);
+  },
+
+  remove: function(element, callback) {
+    $(element).stop().slideUp(500, function() {
+      $(this).remove();
+      
+      if(jQuery.isFunction(callback)) {callback();}
+    });
+  },
+
+  show: function(element, callback) {
+    var e = $(element);
+
+    if(e.is(':visible').length != 0) {
+      e.stop().slideDown(500, callback);
+    }
+  }
+}
+
+var Util = {
+  merge: function(hashOne, hashTwo) {
+    return jQuery.extend({}, hashOne, hashTwo);
+  },
+
+  replaceIds: function(s, regex){
+    return s.replace(regex, new Date().getTime() + State.newIdCounter++);
+  },
+  
+  refreshSortNumbers: function() {
+    $('input.sort_number').val(function(i) { return i + 1; });
+  }
+}
+
+jQuery(function($) {
+  var eventList = $.map(EventHandler, function(v, k ) {return k;});
+  
+  // For old browsers without HTML5 support
+  //$('*[autofocus]:visible:first').focus();
+  
+  $('a[data-event]').live('click', function(event) {
+    if (event.stopped) return;
+    var element = $(this);
+    var eventName = element.data('event');
+
+    if($.inArray(eventName, eventList) != -1) {
+      EventHandler[eventName](element);
+      
+      event.preventDefault();
+      event.stopPropagation();
+    }
+  });
+});
