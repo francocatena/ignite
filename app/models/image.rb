b9fa67ca221f982096f69551a659bcc32739c254
Franco Catena <francocatena@gmail.com>
2011-10-22 01:13:20 -0300

    Correcciones en imágenes

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,12 +1,11 @@
 class Image < ActiveRecord::Base
-  has_attached_file :image, styles: lambda { |attachment|
-    attachment.instance.styles
-  }
+  has_attached_file :image, styles: ->(attachment) {attachment.instance.styles}
   
   # Restricciones
   validates :name, :caption, presence: true, length: { maximum: 255 }
   validates :name, uniqueness: true
-  validates_attachment_presence :image, message: ::I18n.t('errors.messages.blank')
+  validates_attachment_presence :image,
+    message: -> { ::I18n.t('errors.messages.blank') }
   validates_attachment_content_type :image, content_type: /^image\//i
   
   def styles
