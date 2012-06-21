9cdadee9a649e9d403794ac465a4e69afa1ea637
Franco Catena <francocatena@gmail.com>
2012-06-21 12:25:57 -0300

    Little CSS improvements in slides

--- config/routes.rb
+++ config/routes.rb
@@ -11,8 +11,10 @@ Ignite::Application.routes.draw do
     resources :lessons, path: 'l'
   end
   
-  match 'slides/execute_ruby' => 'slides#execute_ruby', as: :execute_ruby,
-    via: :post
+  match(
+    'slides/execute_ruby' => 'slides#execute_ruby',
+    as: 'execute_ruby', via: :post
+  )
   
   root to: 'courses#index'
 end
